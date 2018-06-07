4e4ee532a6a8a18be8e0724ee907fc7c150aa4b2
Meilyn <[andrade.meiyn@gmail.com]>
2018-06-07 09:42:19 +0200

    Javacript premier serie

---
+++ assets/script.js
@@ -0,0 +1,58 @@
+
+// Première série d'exercice -->
+
+/* Exercice 1
+
+var miNom = "Meilyn"
+alert(miNom); */
+
+
+/*Exercice 2 
+
+var myFirstName = "Meilyn";
+var mySecondName = "Andrade";
+var myCity = "Ixelles";
+alert("Nom: " + myFirstName + "Prénom: " + mySecondName + "Ville: " + myCity);*/
+
+/*Exercice 3 
+
+var nom = prompt("Quel est est votre nom?")
+alert("Bonjour " + nom + " !")*/
+
+
+/*Exercice 4 
+
+var nom = prompt("Veuillez rentrer votre nom");
+var prenom = prompt("Veuillez rentre votre Prènom");
+var ville = prompt("Veuillez rentre votre ville");
+
+alert("Nom: " + nom + "Prènom: " + prenom + "Ville: " + ville); */
+
+
+/*Exercice 5 */
+
+/*Exercice 6
+
+var num1 = prompt('Premier nombre ?');
+var num2 = prompt('Deuxieme nombre ?');
+var resultat = parseInt(num1) * parseInt(num2);
+
+ alert("Le résultat est " + resultat);*/
+
+/*
+Exercice 7 
+var pointure = prompt("Quelle est votre pointure?");
+var date = prompt("Quel est votre année de naissance?");
+var resultat = alert(((((pointure * 2) + 5 ) * 50 ) - date ) + 1766 )*/
+
+
+/* Exercice 8
+
+
+ var age = prompt("Quel est votre âge?")
+  if (age > 18) {
+    alert("Vous êtes majeur !")
+  }
+  else {
+    alert("Vous êtes mineur !")
+  } */
