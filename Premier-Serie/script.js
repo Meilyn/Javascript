909306bdc8e4e56085ee0f57e0199a664cad293d
Meilyn <[andrade.meiyn@gmail.com]>
2018-06-19 18:06:23 +0200

    Exercices

---
+++ Premier-Serie/script.js
@@ -0,0 +1,36 @@
+//Exercice 1 
+/*
+let name = "Meilyn";
+alert(name);
+*/
+
+//Exercice 2 :
+/*
+let nom = "Doe";
+prenom = "John";
+ville = "New-York";
+alert("Nom: "+ nom + "\n" + "Prénom: " + prenom + "\n" + "Ville: " + ville);
+*/
+
+//Exercice 3 
+/*
+let nom = prompt ('Veuillez introduire votre nom');
+alert("Bonjour " + nom);
+*/
+
+//Exercice 4 :
+/*
+let nom = prompt("Veuillez introduire votre Nom");
+prenom = prompt("Veuillez introduire votre Prénom");
+ville = prompt("Veuillez introduire votre ville");
+alert("Nom: "+ nom + "\n" + "Prénom: " + prenom + "\n" + "Ville: " + ville);
+*/
+
+//Exercice 5 :
+/*
+let num1 = prompt("Entrez un numero decimal");
+	num2 = prompt("Entrez un 2éme numero decimal");
+
+	result = parseInt(num1) * parseInt(num2);
+	alert(result);	
+	*/
