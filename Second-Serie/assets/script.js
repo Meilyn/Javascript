091e151803f39514dd66f2a02a0ed6a45ecbc1b8
Meilyn <[andrade.meiyn@gmail.com]>
2018-06-21 10:45:28 +0200

    Second serie

---
+++ Second-Serie/assets/script.js
@@ -0,0 +1,27 @@
+//Exercice 1 
+// function bt () {
+// 	long = prompt("Entrez la longueur");
+// 	lang = prompt("Entrez la largeur");
+// 	alert(long * lang);
+// } 
+
+
+
+// Exercise 2
+// function bt() {
+// 	let rayon = parseInt(prompt("Saisisez Rayon"));
+// 	alert((rayon**2) * 3.14);
+// }
+
+//Exercice 3
+let a = 3;
+b = 2; 
+
+function triple(x){
+	return x * 3;
+}
+
+function affiche(){
+	alert(triple(a));
+	alert(triple(b));
+}
