bae5060e392babeaa66f6126990ed0be61fa7ac0
Meilyn <[andrade.meiyn@gmail.com]>
2018-06-21 16:39:09 +0200

    Seconde-Serie

--- Second-Serie/assets/script.js
+++ Second-Serie/assets/script.js
@@ -14,14 +14,55 @@
 // }
 
 //Exercice 3
-let a = 3;
-b = 2; 
+// let a = 3;
+// b = 2; 
 
-function triple(x){
-	return x * 3;
+// function triple(x){
+// 	return x * 3;
+// }
+
+// function affiche(){
+// 	alert(triple(a));
+// 	alert(triple(b));
+// }
+
+//Calculitrice//
+// function Meilyn(){
+// let	a =parseInt(prompt("Entrez votre premier numerà"));
+// 	b =parseInt(prompt("Entrez 2éme numero"));
+// 	x = a + b;
+// 	alert("Vous avez " + x + " Hamburguers"); 
+// }
+
+// Meilyn()
+
+// Exercice #4
+
+/*let tab = [-2 , 1 , 4];
+
+function addittionel(x){
+  return x + 2;
+} 
+function affiche(){
+	alert(addittionel(tab[0]));
+	alert(addittionel(tab[1]));
+ 	alert(addittionel(tab[2]));
+}
+*/
+// Exercice 6 - appel à une fonction retournant une valeur
+
+let tab = [-2, 1, 4];
+	first = [0];
+	last = [tab.length - 1] 
+function soustrait(x){
+  return Math.sign(-2)
+
 }
 
 function affiche(){
-	alert(triple(a));
-	alert(triple(b));
+alert(soustrait(first));
+alert(soustrait(last));
 }
+
+
+
